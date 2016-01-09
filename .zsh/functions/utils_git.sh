766794f778138cf585964aea4d60658bc9618a63
Tetsuro Higuchi <higuchi.tetsuro@gmail.com>
2016-01-09 14:23:28 +0900

    remove zsh-completions submodule and migrate under antigen

--- .zsh/functions/utils_git.sh
+++ .zsh/functions/utils_git.sh
@@ -16,3 +16,13 @@ function gitcoo () {
   git subup
   xcode
 }
+
+function gitrm-submodule() {
+  if [ $1 ]; then
+    git submodule deinit $1
+    git rm $1
+    git config -f .gitmodules --remove-section submodule.$1
+  else
+    echo 'target is not found'
+  fi
+}
